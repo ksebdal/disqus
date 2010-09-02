0cdfbf2fd4a361c35c35aed224ae8e0d0a007a25
k saebdal <ksaebdal@device-e14c2e.(none)>
2010-09-02 10:24:43 +0200

    more data

--- backup.rb
+++ backup.rb
@@ -6,26 +6,36 @@ x = Builder::XmlMarkup.new(:target => $stdout, :indent => 1)
 x.instruct!
 con = Curl::Easy.new('http://disqus.com/api/get_forum_list/?user_api_key=' + ARGV[0] + '&api_version=1.1')
 con.max_redirects = 4
-con.perform
-forum_names = ''
-forums = JSON.parse(con.body_str)
-x.sites  do
-if forums['succeeded']
-  forums['message'].each do 
-    |forum|
-    x.site(:name => forum['name']) do
-      x.articles do
-        con_posts = Curl::Easy.new('http://disqus.com/api/get_forum_posts/?user_api_key=' + ARGV[0] + '&forum_id=' + forum['id'] + '&api_version=1.1')
-        con_posts.max_redirects = 4
-        con_posts.perform
-        posts = JSON.parse(con_posts.body_str)
-        posts['message'].each do
-          |post|
-          x.article(:message => post['message'])
+con.on_success{
+  forum_names = ''
+  forums = JSON.parse(con.body_str)
+  x.sites  do
+    if forums['succeeded']
+      forums['message'].each do 
+        |forum|
+        x.site(:name => forum['name']) do
+          x.comments do
+            con_posts = Curl::Easy.new('http://disqus.com/api/get_forum_posts/?user_api_key=' + ARGV[0] + '&forum_id=' + forum['id'] + '&api_version=1.1')
+            con_posts.max_redirects = 4
+            con_posts.on_success{
+              posts = JSON.parse(con_posts.body_str)
+              posts['message'].each do
+                |post|
+                x.comment do
+                  x.message(post['message'])
+                  x.status(post['status'])
+                  x.ip(post['ip_address'])
+                  x.created(post['created_at'])
+                  x.author(post['anonymous_author'])
+                end
+              end
+            }
+            con_posts.perform
         end
       end
     end
   end
 end
-end
+}
+con.perform
 
